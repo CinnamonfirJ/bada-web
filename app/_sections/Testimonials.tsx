97701f39b9edf6ab3f46050737450b4f001bb2d0
CinnamonfirJ <cinnamonfir@gmail.com>
2025-01-03 08:44:04 +0100

    changes to Testimonials ui

--- app/_sections/Testimonials.tsx
+++ app/_sections/Testimonials.tsx
@@ -71,7 +71,10 @@ export default function Testimonials() {
         </p>
       </div>
 
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-fr'>
+      <div
+        className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-start'
+        style={{ gridTemplateRows: "masonry" }}
+      >
         {testimonials.map((testimonial) => (
           <div
             key={testimonial.id}
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import WhyUseBada from "./_sections/WhyUseBada";
 
 export default function Home() {
   return (
-    <div className='relative'>
+    <div className='relative bg-[#E6E6E6]'>
       {/* Gradient */}
       <div className='absolute top-0 left-0 w-full -z-50 h-48 bg-gradient-to-b from-[rgba(33,144,195,0.3)] to-[rgba(33,144,195,0)] '></div>
       {/* Navbar */}
